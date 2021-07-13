ed28f4d0b78a4995da710792d022bade9c9baea8
Juliette Lamarche <juliette.lamarche@princesse.xyz>
2021-07-13 15:29:49 -0400

    complete rewrite!

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-Port of [nnn](https://github.com/jarun/nnn)'s _type-to-nav_ to [xplr](https://github.com/sayanarijit/xplr) with some tweaks.
+Port of [nnn](https://github.com/jarun/nnn)'s _type-to-nav_ mode for [xplr](https://github.com/sayanarijit/xplr), with some tweaks.
 
 ## Features
 
-Activate this mode and start to type the beginning of relative path. Current directory is filtered accordingly. You can press complete (`tab`) when all the remaining entries stats with the same prefix. When only one entry remains, if it is a directory this directory is focused, filter is reset and you can continue navigating. If it is a file, `type-to-nav` mode exits, focusing on that file (the behavior can be changed).
+Activate _type-to-nav_ mode and start to type the beginning of relative path. Current directory is filtered accordingly. You can press complete (`tab`) when all the remaining entries starts with the same prefix. When only one entry remains, if it is a directory this directory is moved into, filter is reset and you can continue navigating. If it is a file, _type-to-nav_ mode exits, focusing on that file.
 
 ## Installation
 
@@ -31,42 +31,58 @@ Activate this mode and start to type the beginning of relative path. Current dir
 
   require("type-to-nav").setup {
     default_bindings = false,
-    -- action triggered when there is only one path in selection and it is a file
-    -- or when `custom.type_to_nav_accept` is called when focus is on a file
-    accept = { { CallLuaSilently = "custom.type_to_nav_quit_focus" } },
   }
-  xplr.config.modes.builtin.default.key_bindings.on_key["ctrl-n"] = {
-    help = "type-to-nav",
+
+  -- this reproduces the defaults, work it from there!
+  xplr.config.modes.builtin.default.key_bindings.on_key['ctrl-n'] = {
+    help = 'type-to-nav',
     messages = {
-      { SwitchModeCustom = "type_to_nav" },
+      { CallLuaSilently = 'custom.type_to_nav_start' },
     },
   }
-  merge_in(xplr.config.modes.custom.type_to_nav.key_bindings.on_key["esc"] = {
-    help = "quit mode",
-    messages = { { CallLuaSilently = "custom.type_to_nav_quit" } },
-  }
-  merge_in(xplr.config.modes.custom.type_to_nav.key_bindings.on_key["backspace"] = {
-    help = "backspace",
-    messages = { { CallLuaSilently = "custom.type_to_nav_backspace" } },
-  }
-  merge_in(xplr.config.modes.custom.type_to_nav.key_bindings.on_key["tab"] = {
-    help = "complete",
-    messages = { { CallLuaSilently = "custom.type_to_nav_complete" } },
-  }
-  merge_in(xplr.config.modes.custom.type_to_nav.key_bindings.on_key["enter"] = {
-    help = "accept",
-    message = { { CallLuaSilently = "custom.type_to_nav_accept" } },
-  }
+  merge_in(xplr.config.modes.custom.type_to_nav.key_bindings.on_key, {
+    esc = {
+      help = 'quit mode',
+      messages = { { CallLuaSilently = 'custom.type_to_nav_quit' } },
+    },
+    ['ctrl-u'] = {
+      help = 'clear input',
+      messages = { { CallLuaSilently = 'custom.type_to_nav_clear_input' } },
+    },
+    ['ctrl-h'] = {
+      help = 'up',
+      messages = { { CallLuaSilently = 'custom.type_to_nav_up' } },
+    },
+    backspace = {
+      help = 'remove last character',
+      messages = {
+        { CallLuaSilently = 'custom.type_to_nav_remove_last_character' },
+      },
+    },
+    tab = {
+      help = 'complete',
+      messages = { { CallLuaSilently = 'custom.type_to_nav_complete' } },
+    },
+    enter = {
+      help = 'accept',
+      messages = { { CallLuaSilently = 'custom.type_to_nav_accept' } },
+    },
+    ['ctrl-p'] = { help = 'focus previous', messages = { 'FocusPrevious' } },
+    ['ctrl-n'] = { help = 'focus next', messages = { 'FocusNext' } },
+    ['ctrl-s'] = { help = 'toggle select', messages = { 'ToggleSelection' } },
+    ['ctrl-a'] = {
+      help = 'toggle select all',
+      messages = { 'ToggleSelectAll' },
+    },
+  })
   ```
-
-  -- this setup reproduces the defaults, work it from there!
 
 ## TODO
 
 - Deal with symbolic links.
-- Add action to clear filter (so you can move up in path).
+
+## Maybe
+
 - Support automatic completion instead having to press `tab` manually (and symmetric behavior for `backspace`)
-- Refuse types that leads to empty selection.
-- Restore dot filter if it was automatically removed.
-- Improve filter handling in general.
-- Bring unicode support. (I don't think this can be achieved in the current state of xplr's API)
+- Refuse key strokes that leads to empty selections.
+- Select submode where on accepting a file, the file is added to selection, the input is clear but _type-to-nav_ mode does not exit.
--- src/init.lua
+++ src/init.lua
@@ -1,247 +1,288 @@
 local M = {}
 
-local function join(t1, t2)
-	local res = {}
-	for _, t in pairs(t1) do
-		table.insert(res, t)
-	end
-	for _, t in pairs(t2) do
-		table.insert(res, t)
-	end
-	return res
-end
-
 local function merge_in(t1, t2)
-	for k, v in pairs(t2) do
-		t1[k] = v
-	end
+  for k, v in pairs(t2) do
+    t1[k] = v
+  end
 end
 
 local function longest_common_chain(a, b)
-	local j = 1
-	while true do
-		if j == #a + 1 then
-			break
-		end
-		if j == #b + 1 then
-			break
-		end
-		if a:sub(j, j) ~= b:sub(j, j) then
-			break
-		end
-		j = j + 1
-	end
-	return a:sub(1, j - 1)
+  local j = 1
+  while true do
+    if j == #a + 1 then
+      break
+    end
+    if j == #b + 1 then
+      break
+    end
+    if a:sub(j, j) ~= b:sub(j, j) then
+      break
+    end
+    j = j + 1
+  end
+  return a:sub(1, j - 1)
 end
 
-local prefix = ""
 local opts = {
-	on_one_file_messages = {
-		{ LogInfo = "triggered" },
-		{ CallLuaSilently = "custom.type_to_nav_quit_focus" },
-	},
-	default_bindings = true,
+  default_bindings = true,
 }
 
-xplr.fn.custom.type_to_nav_quit = function()
-	local old = prefix
-	prefix = ""
-	return {
-		{ RemoveNodeFilter = {
-			filter = "RelativePathDoesStartWith",
-			input = old,
-		} },
-		"ExplorePwd",
-		"PopMode",
-		{ SwitchModeBuiltin = "default" },
-	}
+local is_dot_filter
+
+xplr.fn.custom.type_to_nav_rebuf = function(app)
+  if #app.input_buffer > 0 then
+    local count = 0
+    local node
+    for _, node0 in ipairs(app.directory_buffer.nodes) do
+      local p = node0.relative_path
+      -- check if node's relative_path starts with input buffer
+      if p:sub(1, #app.input_buffer) == app.input_buffer then
+        count = count + 1
+        node = node0
+      end
+    end
+    if count == 1 then
+      local messages = {}
+      if node.is_dir then
+        table.insert(
+          messages,
+          { CallLuaSilently = 'custom.type_to_nav_reset_filters' }
+        )
+        table.insert(messages, { ChangeDirectory = node.absolute_path })
+        table.insert(messages, { SetInputBuffer = '' })
+        table.insert(messages, 'ExplorePwdAsync')
+      elseif node.is_file then
+        table.insert(messages, { CallLuaSilently = 'custom.type_to_nav_quit' })
+      end
+      return messages
+    end
+  end
+  local messages = {}
+  for _, filter in ipairs(app.explorer_config.filters) do
+    if filter.filter == 'RelativePathDoesStartWith' then
+      table.insert(messages, {
+        RemoveNodeFilter = {
+          filter = 'RelativePathDoesStartWith',
+          input = filter.input,
+        },
+      })
+    end
+  end
+  if app.input_buffer == '.' then
+    table.insert(messages, {
+      RemoveNodeFilter = {
+        filter = 'RelativePathDoesNotStartWith',
+        input = '.',
+      },
+    })
+  end
+  table.insert(messages, {
+    AddNodeFilter = {
+      filter = 'RelativePathDoesStartWith',
+      input = app.input_buffer,
+    },
+  })
+  table.insert(messages, 'ExplorePwdAsync')
+  return messages
 end
 
-xplr.fn.custom.type_to_nav_quit_focus = function(app)
-	local focused_node = app.focused_node
-	if not focused_node then
-		return xplr.fn.custom.type_to_nav_quit()
-	end
-	local old = prefix
-	prefix = ""
-	return join({ { FocusPath = focused_node.absolute_path } }, xplr.fn.custom.type_to_nav_quit())
+xplr.fn.custom.type_to_nav_clear_input = function()
+  local messages = {}
+  table.insert(
+    messages,
+    { CallLuaSilently = 'custom.type_to_nav_reset_filters' }
+  )
+  table.insert(messages, { SetInputBuffer = '' })
+  table.insert(messages, { CallLuaSilently = 'custom.type_to_nav_rebuf' })
+  return messages
+end
+
+xplr.fn.custom.type_to_nav_up = function()
+  return {
+    { SetInputBuffer = '' },
+    { ChangeDirectory = '..' },
+    { CallLuaSilently = 'custom.type_to_nav_rebuf' },
+  }
 end
 
 xplr.fn.custom.type_to_nav_complete = function(app)
-	if not app.directory_buffer then
-		return
-	end
-	prefix = nil
-	for _, node in ipairs(app.directory_buffer.nodes) do
-		local p = node.relative_path
-		if not prefix then
-			prefix = p
-		else
-			prefix = longest_common_chain(prefix, p)
-		end
-	end
-	local old = prefix
-	return {
-		{ RemoveNodeFilter = {
-			filter = "RelativePathDoesStartWith",
-			input = old,
-		} },
-		{ AddNodeFilter = {
-			filter = "RelativePathDoesStartWith",
-			input = prefix,
-		} },
-		"ExplorePwdAsync",
-	}
+  if not app.directory_buffer then
+    return
+  end
+  local input = nil
+  for _, node in ipairs(app.directory_buffer.nodes) do
+    local p = node.relative_path
+    if not input then
+      input = p
+    else
+      input = longest_common_chain(input, p)
+    end
+  end
+  return {
+    { SetInputBuffer = input },
+    { CallLuaSilently = 'custom.type_to_nav_rebuf' },
+  }
 end
 
-xplr.fn.custom.type_to_nav_backspace = function()
-	if #prefix == 0 then
-		return
-	end
-	local old = prefix
-	prefix = prefix:sub(1, -2)
-	return {
-		{ RemoveNodeFilter = {
-			filter = "RelativePathDoesStartWith",
-			input = old,
-		} },
-		{ AddNodeFilter = {
-			filter = "RelativePathDoesStartWith",
-			input = prefix,
-		} },
-		"ExplorePwdAsync",
-	}
-end
-
-xplr.fn.custom.type_to_nav_accept = function(app)
-	local focus = app.directory_buffer.focus
-	local node = app.directory_buffer.nodes[focus]
-	if node.is_dir then
-		local old = prefix
-		prefix = ""
-		return {
-			{ RemoveNodeFilter = {
-				filter = "RelativePathDoesStartWith",
-				input = old,
-			} },
-			{ ChangeDirectory = node.absolute_path },
-		}
-	elseif node.is_file then
-		local old = prefix
-		prefix = ""
-		return join(opts.on_one_file_messages, {
-			{ RemoveNodeFilter = {
-				filter = "RelativePathDoesStartWith",
-				input = old,
-			} },
-			"ExplorePwdAsync",
-		})
-	end
-end
-
-xplr.fn.custom.type_to_nav_check_dir = function(app)
-	local card
-	if app.directory_buffer then
-		card = #app.directory_buffer.nodes
-	else
-		card = 0
-	end
-	if card == 1 then
-		local node = app.directory_buffer.nodes[1]
-		if node.is_dir then
-			local old = prefix
-			prefix = ""
-			return {
-				{ RemoveNodeFilter = {
-					filter = "RelativePathDoesStartWith",
-					input = old,
-				} },
-				{ ChangeDirectory = node.absolute_path },
-			}
-		elseif node.is_file then
-			local old = prefix
-			prefix = ""
-			return join(opts.on_one_file_messages, {
-				{ RemoveNodeFilter = {
-					filter = "RelativePathDoesStartWith",
-					input = old,
-				} },
-				"ExplorePwdAsync",
-			})
-		end
-	end
+xplr.fn.custom.type_to_nav_remove_last_character = function(app)
+  local input = app.input_buffer
+  if #input == 0 then
+    return
+  end
+  input = input:sub(1, -2)
+  return {
+    { SetInputBuffer = input },
+    { CallLuaSilently = 'custom.type_to_nav_rebuf' },
+  }
 end
 
 xplr.config.modes.custom.type_to_nav = {
-	name = "type-to-nav",
-	key_bindings = {
-		on_key = {},
-	},
+  name = 'type-to-nav',
 }
-for i = 32, 126 do
-	local key = string.char(i)
-	local identifier = "type_to_nav_press_" .. tostring(i)
-	xplr.fn.custom[identifier] = function(app)
-		local old = prefix
-		prefix = prefix .. key
-		local messages
-		if prefix == "." then
-			messages = {
-				{ RemoveNodeFilter = { filter = "RelativePathDoesNotStartWith", input = "." } },
-			}
-		else
-			messages = {}
-		end
-		return join(messages, {
-			{ RemoveNodeFilter = {
-				filter = "RelativePathDoesStartWith",
-				input = old,
-			} },
-			{ AddNodeFilter = {
-				filter = "RelativePathDoesStartWith",
-				input = prefix,
-			} },
-			"ExplorePwd",
-			{ CallLuaSilently = "custom.type_to_nav_check_dir" },
-		})
-	end
-	xplr.config.modes.custom.type_to_nav.key_bindings.on_key[key] = {
-		messages = {
-			{ CallLuaSilently = "custom." .. identifier },
-		},
-	}
+
+xplr.fn.custom.type_to_nav_start = function(app)
+  local messages = {
+    { SwitchModeCustom = 'type_to_nav' },
+    { BufferInput = '' },
+  }
+  is_dot_filter = false
+  for _, filter in ipairs(app.explorer_config.filters) do
+    if
+      filter.filter == 'RelativePathDoesNotStartWith' and filter.input == '.'
+    then
+      is_dot_filter = true
+    elseif filter.filter == 'RelativePathDoesStartWith' then
+      table.insert(messages, {
+        RemoveNodeFilter = {
+          filter = 'RelativePathDoesStartWith',
+          input = filter.input,
+        },
+      })
+    end
+  end
+  table.insert(messages, 'ExplorePwdAsync')
+  return messages
 end
 
+xplr.fn.custom.type_to_nav_reset_filters = function(app)
+  local messages = {}
+  if is_dot_filter then
+    table.insert(messages, {
+      AddNodeFilter = {
+        filter = 'RelativePathDoesNotStartWith',
+        input = '.',
+      },
+    })
+  end
+  for _, filter in ipairs(app.explorer_config.filters) do
+    if filter.filter == 'RelativePathDoesStartWith' then
+      table.insert(messages, {
+        RemoveNodeFilter = {
+          filter = 'RelativePathDoesStartWith',
+          input = filter.input,
+        },
+      })
+    end
+  end
+  return messages
+end
+
+xplr.fn.custom.type_to_nav_quit = function(app)
+  local messages = {}
+  table.insert(
+    messages,
+    { CallLuaSilently = 'custom.type_to_nav_reset_filters' }
+  )
+  table.insert(
+    messages,
+    { CallLuaSilently = 'custom.type_to_nav_reset_filters' }
+  )
+  table.insert(messages, 'ExplorePwdAsync')
+  table.insert(messages, 'PopMode')
+  table.insert(messages, { SwitchModeBuiltin = 'default' })
+  return messages
+end
+
+-- TODO deal with symlinks
+xplr.fn.custom.type_to_nav_accept = function(app)
+  local node = app.focused_node
+  if node.is_dir then
+    local messages = {}
+    table.insert(
+      messages,
+      { CallLuaSilently = 'custom.type_to_nav_reset_filters' }
+    )
+    table.insert(messages, { SetInputBuffer = '' })
+    table.insert(messages, { ChangeDirectory = node.absolute_path })
+    table.insert(messages, {
+      CallLuaSilently = 'custom.type_to_nav_rebuf',
+    })
+    return messages
+  end
+  return xplr.fn.custom.type_to_nav_quit(app)
+end
+
+local char_bindings = {
+  messages = {
+    'BufferInputFromKey',
+    { CallLuaSilently = 'custom.type_to_nav_rebuf' },
+  },
+}
+
+xplr.config.modes.custom.type_to_nav.key_bindings = {
+  on_alphabet = char_bindings,
+  on_number = char_bindings,
+  on_special_character = char_bindings,
+  on_key = {},
+}
+
 M.setup = function(lopts)
-	if lopts then
-		merge_in(opts, lopts)
-	end
-	if opts.default_bindings then
-		xplr.config.modes.builtin.default.key_bindings.on_key["ctrl-n"] = {
-			help = "type-to-nav",
-			messages = {
-				{ SwitchModeCustom = "type_to_nav" },
-			},
-		}
-		merge_in(xplr.config.modes.custom.type_to_nav.key_bindings.on_key, {
-			esc = {
-				help = "quit mode",
-				messages = { { CallLuaSilently = "custom.type_to_nav_quit" } },
-			},
-			backspace = {
-				help = "backspace",
-				messages = { { CallLuaSilently = "custom.type_to_nav_backspace" } },
-			},
-			tab = {
-				help = "complete",
-				messages = { { CallLuaSilently = "custom.type_to_nav_complete" } },
-			},
-			enter = {
-				help = "accept",
-				messages = { { CallLuaSilently = "custom.type_to_nav_accept" } },
-			},
-		})
-	end
+  if lopts then
+    merge_in(opts, lopts)
+  end
+  if opts.default_bindings then
+    xplr.config.modes.builtin.default.key_bindings.on_key['ctrl-n'] = {
+      help = 'type-to-nav',
+      messages = {
+        { CallLuaSilently = 'custom.type_to_nav_start' },
+      },
+    }
+    merge_in(xplr.config.modes.custom.type_to_nav.key_bindings.on_key, {
+      esc = {
+        help = 'quit mode',
+        messages = { { CallLuaSilently = 'custom.type_to_nav_quit' } },
+      },
+      ['ctrl-u'] = {
+        help = 'clear input',
+        messages = { { CallLuaSilently = 'custom.type_to_nav_clear_input' } },
+      },
+      ['ctrl-h'] = {
+        help = 'up',
+        messages = { { CallLuaSilently = 'custom.type_to_nav_up' } },
+      },
+      backspace = {
+        help = 'remove last character',
+        messages = {
+          { CallLuaSilently = 'custom.type_to_nav_remove_last_character' },
+        },
+      },
+      tab = {
+        help = 'complete',
+        messages = { { CallLuaSilently = 'custom.type_to_nav_complete' } },
+      },
+      enter = {
+        help = 'accept',
+        messages = { { CallLuaSilently = 'custom.type_to_nav_accept' } },
+      },
+      ['ctrl-p'] = { help = 'focus previous', messages = { 'FocusPrevious' } },
+      ['ctrl-n'] = { help = 'focus next', messages = { 'FocusNext' } },
+      ['ctrl-s'] = { help = 'toggle select', messages = { 'ToggleSelection' } },
+      ['ctrl-a'] = {
+        help = 'toggle select all',
+        messages = { 'ToggleSelectAll' },
+      },
+    })
+  end
 end
 
 return M
